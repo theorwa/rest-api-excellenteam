04dc23b70e9441c912374f84077e39c231f0c4b5
theorwa <theorwa@gmail.com>
2019-09-19 14:17:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -48,9 +48,13 @@ app.use(function(req, res, next) {
 
 // app.listen(9001);
 
+app.listen(process.env.PORT);
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
