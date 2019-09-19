4172069ce93b347f33aef8fd7d6ee85401ef48aa
theorwa <theorwa@gmail.com>
2019-09-19 14:14:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,40 @@ var app = require('express')();
 
 
 
-// // ========== MiddleWares ===========
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());
+// ========== MiddleWares ===========
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-// var cookieParser = require('cookie-parser');
-// app.use(cookieParser());
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-// var errorHandler = require('errorhandler');
-// app.use(errorHandler());
+var errorHandler = require('errorhandler');
+app.use(errorHandler());
 
-// var cors = require('cors');
-// app.use(cors());
-// // ===================================
-
-
-// // ==== Simple Example 1 ====
-// app.get('/simple1',function(req,res){
-//     res.send('Hello World!');
-// });
-// // ==========================
+var cors = require('cors');
+app.use(cors());
+// ===================================
 
 
-// // ==== Simple Example 2 ====
-// app.get('/simple2',function(req,res){
-//     res.write("WELCOME!\n");
-//     res.end('Hello World!');
-// });
-// // ==========================
+// ==== Simple Example 1 ====
+app.get('/simple1',function(req,res){
+    res.send('Hello World!');
+});
+// ==========================
+
+
+// ==== Simple Example 2 ====
+app.get('/simple2',function(req,res){
+    res.write("WELCOME!\n");
+    res.end('Hello World!');
+});
+// ==========================
 
 
 
-// // ==== use external file routes.js ====
-// // app.use('/', require('./routes'))
-// // =====================================
+// ==== use external file routes.js ====
+app.use('/', require('./routes'))
+// =====================================
 
 
 
